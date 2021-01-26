ed876e9c1454e875c4938316027cec3464d1154d
Andres <agaviriat@gmail.com>
2021-01-26 15:22:27 -0500

    12

--- models/crm_lead.py
+++ models/crm_lead.py
@@ -28,4 +28,44 @@ class Lead(models.Model):
             ('3', 'No'),
         ], "2. ¿Cuenta o elabora la empresa presupuesto semanal/ quincenal/ mensual/ trimestral/ semestral/ anual?",
     )
-    
+    x_org3 = fields.Selection(
+        [
+            ('0', 'No Aplica'),
+            ('1', 'Si'),
+            ('2', 'En Proceso'),
+            ('3', 'No'),
+        ], "3. ¿Cuenta la empresa con plan estratégico de desarrollo?",
+    )
+    x_org4 = fields.Selection(
+        [
+            ('0', 'No Aplica'),
+            ('1', 'Si'),
+            ('2', 'En Proceso'),
+            ('3', 'No'),
+        ], "4. ¿Cuenta su empresa con un organigrama publicado en un lugar visible para sus trabajadores?",
+    )
+    x_org5 = fields.Selection(
+        [
+            ('0', 'No Aplica'),
+            ('1', 'Si'),
+            ('2', 'En Proceso'),
+            ('3', 'No'),
+        ], "5. ¿Cuenta la empresa con un manual de procedimientos administrativos y contables?",
+    ) 
+    x_org6 = fields.Selection(
+        [
+            ('0', 'No Aplica'),
+            ('1', 'Si'),
+            ('2', 'En Proceso'),
+            ('3', 'No'),
+        ], "6. ¿Los Socios(as) de la Empresa/ Organización/ Cooperativa, realizan periódicamente sus aportaciones obligatorias según sus estatutos?",
+    ) 
+    x_org7 = fields.Selection(
+        [
+            ('0', 'No Aplica'),
+            ('1', 'Si'),
+            ('2', 'En Proceso'),
+            ('3', 'No'),
+        ], "7. ¿Los Socios(as) de la Empresa/ Organización/ Cooperativa, realizan periódicamente sus aportaciones obligatorias según sus estatutos?",
+    ) 
+
